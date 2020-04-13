df1f4a148c4ac23504c6b7491e64d449e300c787
Juerg Hunziker <juerg.hunziker@liip.ch>
2020-04-13 13:20:42 +0200

    implement test for loginUser with different username/password

--- cypress/integration/login-user.spec.js
+++ cypress/integration/login-user.spec.js
@@ -6,4 +6,9 @@ context('cy.loginUser()', () => {
 		cy.url().should('include', '/wp-admin')
 		cy.url().should('not.include', '/wp-login.php')
 	})
+
+	it('should login user with different username and password', () => {
+		cy.loginUser('otheruser', 'otherpass')
+		cy.get('#login_error').contains('Unknown username. Check again or try your email address.')
+	})
 })
