950caff5ca755340b604ffddff64f6800fe899c8
Juerg Hunziker <juerg.hunziker@liip.ch>
2021-12-03 21:49:49 +0100

    remove usage of wpConfig option

--- dist/commands/open-global-block-inserter.js
+++ dist/commands/open-global-block-inserter.js
@@ -1,18 +1,16 @@
-import config from '../shared/config';
 /**
  * Opens the global block inserter.
  */
 export const openGlobalBlockInserter = () => {
-    const toggleBlockInserterButtonSelector = config.wpVersion >= 5.8
-        ? '.edit-post-header [aria-label="Toggle block inserter"]'
-        : '.edit-post-header [aria-label="Add block"]';
-    const inserterPanelSelector = config.wpVersion >= 5.8
-        ? '.edit-post-editor__inserter-panel'
-        : '.edit-post-layout__inserter-panel';
+    const toggleBlockInserterButtonSelector57 = '//div[contains(@class,"edit-post-header")]//button[contains(@aria-label,"Add block")]';
+    const toggleBlockInserterButtonSelector = '//div[contains(@class,"edit-post-header")]//button[contains(@aria-label,"Toggle block inserter")]';
+    const inserterPanelSelector57 = '.edit-post-layout__inserter-panel';
+    const inserterPanelSelector = '.edit-post-editor__inserter-panel';
     cy.window().then((window) => {
+        const isGlobalBlockInserterOpen57 = window.document.querySelector(inserterPanelSelector57);
         const isGlobalBlockInserterOpen = window.document.querySelector(inserterPanelSelector);
-        if (!isGlobalBlockInserterOpen) {
-            cy.get(toggleBlockInserterButtonSelector).click({
+        if (!isGlobalBlockInserterOpen && !isGlobalBlockInserterOpen57) {
+            cy.xpath(`${toggleBlockInserterButtonSelector57} | ${toggleBlockInserterButtonSelector}`).click({
                 force: true,
             });
         }
--- dist/commands/search-for-block.js
+++ dist/commands/search-for-block.js
@@ -1,13 +1,13 @@
-import config from '../shared/config';
 /**
  * Search for block in the global inserter.
  *
  * @param {string} searchTerm The text to search the inserter for.
  */
 export const searchForBlock = (searchTerm) => {
-    const searchInputSelector = config.wpVersion >= 5.9
-        ? '.block-editor-inserter__search input'
-        : 'input.block-editor-inserter__search-input';
+    const searchInputSelector58 = '//input[contains(@class,"block-editor-inserter__search-input")]';
+    const searchInputSelector = '//div[contains(@class,"block-editor-inserter__search")]//input';
     cy.openGlobalBlockInserter();
-    cy.get(searchInputSelector).clear().type(searchTerm);
+    cy.xpath(`${searchInputSelector} | ${searchInputSelector58}`)
+        .clear()
+        .type(searchTerm);
 };
