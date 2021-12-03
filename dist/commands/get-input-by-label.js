f83bcc2a3aebd80bdb8473d13a5e688a015049f2
Juerg Hunziker <juerg.hunziker@liip.ch>
2021-12-03 22:46:10 +0100

    add possibility to select input by label element or ariaLabel

--- dist/commands/get-input-by-label.js
+++ dist/commands/get-input-by-label.js
@@ -1,3 +1,6 @@
-export const getInputByLabel = (label) => {
-    return cy.get(`input[aria-label="${label}"]`);
+export const getInputByLabel = (label, labelType = 'ariaLabel') => {
+    if (labelType === 'ariaLabel') {
+        return cy.get(`input[aria-label="${label}"]`);
+    }
+    return cy.xpath(`//input/preceding-sibling::label[contains(text(),"${label}")]`);
 };
--- src/commands/get-input-by-label.ts
+++ src/commands/get-input-by-label.ts
@@ -2,11 +2,19 @@
  * Gets a input element by label.
  *
  * @param {string} label Label of input element.
+ * @param {'ariaLabel' | 'element'} [labelType] The type of button label: 'ariaLabel' or 'element'.
  */
 import Chainable = Cypress.Chainable;
 
 export const getInputByLabel = (
-  label: string
+  label: string,
+  labelType: 'ariaLabel' | 'element' = 'ariaLabel'
 ): Chainable<JQuery<HTMLElement>> => {
-  return cy.get(`input[aria-label="${label}"]`);
+  if (labelType === 'ariaLabel') {
+    return cy.get(`input[aria-label="${label}"]`);
+  }
+
+  return cy.xpath(
+    `//input/preceding-sibling::label[contains(text(),"${label}")]`
+  );
 };
