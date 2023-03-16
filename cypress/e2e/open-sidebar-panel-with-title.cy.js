e2b0c16f9d40b3e2da1e7aa124e8e1915c01d4b9
Juerg Hunziker <juerg.hunziker@liip.ch>
2023-03-16 11:26:31 +0100

    make selector more specific

--- cypress/e2e/open-sidebar-panel-with-title.cy.js
+++ cypress/e2e/open-sidebar-panel-with-title.cy.js
@@ -7,15 +7,14 @@ context('openSidebarPanelWithTitle Command', () => {
   });
 
   it('should open sidebar panel with given title', () => {
-    cy.ensureSidebarOpened();
     if (Cypress.env('wp_version') && Cypress.env('wp_version') < 6.1) {
       cy.openSidebarPanelWithTitle('Permalink');
       cy.get('h3.edit-post-post-link__preview-label').contains('View Post');
     } else {
       cy.openSidebarPanelWithTitle('Categories');
-      cy.get('label.components-checkbox-control__label').contains(
-        'Uncategorized'
-      );
+      cy.get(
+        '.editor-post-taxonomies__hierarchical-terms-choice label.components-checkbox-control__label'
+      ).contains('Uncategorized');
     }
   });
 });
